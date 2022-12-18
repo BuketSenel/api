62cde7fbc8d661a53ba883389b192d0e521adcb5
BuketSenel <buket.senel@ug.bilkent.edu.tr>
2022-12-18 20:52:13 +0300

    routes:: restaurant route added

--- pkg/routes/restaurants_route.go
+++ pkg/routes/restaurants_route.go
@@ -1,7 +1,13 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func restaurantRoute(rg *gin.RouterGroup) {
-
+	rg.GET("/:resId", func(c *gin.Context) {
+		resId := c.Params.ByName("resId")
+		c.JSON(http.StatusOK, gin.H{"resId": resId})
+	})
 }
--- pkg/routes/users_route.go
+++ pkg/routes/users_route.go
@@ -6,7 +6,7 @@ import (
 )
 
 func userRoute(rg *gin.RouterGroup) {
-	rg.GET("/user/:name", func(c *gin.Context) {
+	rg.GET("/:name", func(c *gin.Context) {
 		user := c.Params.ByName("name")
 		c.JSON(http.StatusOK, gin.H{"user": user})
 	})
