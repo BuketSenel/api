7f13e966a07a09b69d76beb2b77277a8d6ae4143
utkbk <utku.bekci@ug.bilkent.edu.tr>
2022-12-25 12:58:38 +0300

    Restaurant Route :: fixed

--- pkg/routes/restaurants_route.go
+++ pkg/routes/restaurants_route.go
@@ -9,25 +9,24 @@ import (
 )
 
 func restaurantRoute(rg *gin.RouterGroup) {
-	rg.Group("/restaurants")
-	/*
-		rg.GET("/:resId", func(c *gin.Context) {
-			resID := c.Param("resId")
-			id, _ := strconv.ParseInt(resID, 16, 64)
-			restaurant := controllers.GetRestaurant(id)
-			if err := c.BindJSON(&restaurant); err != nil || restaurant == nil || len(restaurant) == 0 {
-				c.Header("Content-Type", "application/json")
-				c.JSON(http.StatusNotFound,
-					gin.H{"Error: ": "Invalid startingIndex on search filter!"})
-				c.Abort()
-			} else {
-				c.Header("Content-Type", "application/json")
-				c.IndentedJSON(http.StatusOK, restaurant)
-			}
-		})
-	*/
+	restGroup := rg.Group("/")
 
-	rg.GET("/:resId", func(c *gin.Context) {
+	restGroup.GET("/:resId", func(c *gin.Context) {
+		resID := c.Param("resId")
+		id, _ := strconv.ParseInt(resID, 16, 64)
+		restaurant := controllers.GetRestaurant(id)
+		if restaurant == nil || len(restaurant) == 0 {
+			c.Header("Content-Type", "application/json")
+			c.JSON(http.StatusNotFound,
+				gin.H{"Error: ": "Invalid startingIndex on search filter!"})
+			c.Abort()
+
+		} else {
+			c.IndentedJSON(http.StatusOK, restaurant)
+		}
+	})
+
+	restGroup.GET("/:resId/categories", func(c *gin.Context) {
 		resID := c.Param("resId")
 		id, _ := strconv.ParseInt(resID, 16, 64)
 		restaurant := controllers.GetRestaurant(id)
@@ -40,20 +39,7 @@ func restaurantRoute(rg *gin.RouterGroup) {
 		}
 	})
 
-	rg.GET("/:resId/categories", func(c *gin.Context) {
-		resID := c.Param("resId")
-		id, _ := strconv.ParseInt(resID, 16, 64)
-		restaurant := controllers.GetRestaurant(id)
-		if err := c.BindJSON(&restaurant); err != nil || restaurant == nil || len(restaurant) == 0 {
-			fmt.Println(err)
-			fmt.Println(restaurant)
-			c.AbortWithStatus(http.StatusNotFound)
-		} else {
-			c.IndentedJSON(http.StatusOK, restaurant)
-		}
-	})
-
-	rg.GET("/all", func(c *gin.Context) {
+	restGroup.GET("/all", func(c *gin.Context) {
 		restaurant := controllers.GetTopRestaurants()
 		if restaurant == nil || len(restaurant) == 0 {
 			c.Header("Content-Type", "application/json")
