7fd94be209fbe34a2cb8e3d18244b2ff61cba2d2
utkbk <75905276+utkbk@users.noreply.github.com>
2023-03-05 19:43:50 +0300

    User :: registration routing simplified

--- pkg/routes/users_route.go
+++ pkg/routes/users_route.go
@@ -1,13 +1,36 @@
 package routes
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/SelfServiceCo/api/pkg/controllers"
+	"github.com/gin-gonic/gin"
 )
 
 func userRoute(rg *gin.RouterGroup) {
 	rg.GET("/:name", func(c *gin.Context) {
 		user := c.Params.ByName("name")
 		c.JSON(http.StatusOK, gin.H{"user": user})
+	})
+
+	rg.POST("/register", func(c *gin.Context) {
+		register := controllers.UserRegister(c)
+		if !register {
+			c.Header("Content-Type", "application/json")
+			c.JSON(http.StatusNotFound,
+				gin.H{
+					"status":    http.StatusNotFound,
+					"message: ": "User registration failed!",
+				})
+			c.Abort()
+		} else {
+			c.JSON(http.StatusOK, gin.H{
+				"status":  "200",
+				"message": "OK",
+				"items":   register,
+				"offset":  "0",
+				"limit":   "25",
+			})
+		}
 	})
 }
