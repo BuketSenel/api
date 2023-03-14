5a374db5e1e73b739b2f4f263ae4a37eddab3d27
utkbk <75905276+utkbk@users.noreply.github.com>
2023-03-14 21:03:01 +0300

    Fix import

--- pkg/middleware/jwt.middleware.go
+++ pkg/middleware/jwt.middleware.go
@@ -3,13 +3,13 @@ package middleware
 import (
 	"net/http"
 
-	"github.com/SelfServiceCo/api/pkg/token"
+	"github.com/SelfServiceCo/api/pkg/controllers"
 	"github.com/gin-gonic/gin"
 )
 
 func JwtAuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		err := token.TokenValidation(c)
+		err := controllers.TokenValidation(c)
 		if err != nil {
 			c.String(http.StatusUnauthorized, "Unauthorized")
 			c.Abort()
