065229f21cafef9d44d78704ae398da306d7d77f
BuketSenel <buket.senel@ug.bilkent.edu.tr>
2023-04-01 13:16:08 +0300

    Menu Creation :: Create Category function added

--- pkg/controllers/category_controller.go
+++ pkg/controllers/category_controller.go
@@ -3,8 +3,10 @@ package controllers
 import (
 	"database/sql"
 	"fmt"
+	"net/http"
 
 	"github.com/SelfServiceCo/api/pkg/models"
+	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -62,4 +64,27 @@ func CategoriesByRestaurant(rid int64) []models.Category {
 	}
 
 	return categories
+}
+
+func CreateCategory(c *gin.Context) (bool, gin.H) {
+	category := models.Category{}
+	err := c.BindJSON(&category)
+	if err != nil {
+		return false, gin.H{"status": http.StatusBadRequest, "message": "Bind Error! Create Product Category"}
+	}
+
+	db, err := sql.Open("mysql", conf.Name+":"+conf.Password+"@tcp("+conf.Db+":3306)/selfservice")
+
+	if err != nil {
+		return false, gin.H{"status": http.StatusBadRequest, "message": "DB Connection Error! Create Product Category"}
+	}
+
+	results, err := db.Query("INSERT INTO categories (cat_name, rest_id, cat_desc, cat_image, parent_cat_id) VALUES (?,?,?,?,?)", category.Name, category.RID, category.Description, category.Image, category.ParentCatID)
+	defer db.Close()
+
+	if err != nil {
+		return false, gin.H{"status": http.StatusBadRequest, "message": "Insertion Error! Create Product Category"}
+	}
+
+	return true, gin.H{"status": http.StatusOK, "message": "Product category created!", "data": results}
 }
--- pkg/models/category.go
+++ pkg/models/category.go
@@ -4,7 +4,7 @@ type Category struct {
 	ID          int64  `json:"cat_id"`
 	RID         int64  `json:"rest_id"`
 	Name        string `json:"cat_name"`
-	Description string `json:"cat_description"`
+	Description string `json:"cat_desc"`
 	Image       string `json:"cat_image"`
 	ParentCatID int16  `json:"parent_cat_id"`
 }
