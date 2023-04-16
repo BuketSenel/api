ca3f0502d05b06873f2e3e6679f1809182a118b5
BuketSenel <buket.senel@ug.bilkent.edu.tr>
2023-04-16 15:58:01 +0300

    Table :: Create, Get Table functions added

--- pkg/controllers/table_controller.go
+++ pkg/controllers/table_controller.go
@@ -3,8 +3,10 @@ package controllers
 import (
 	"database/sql"
 	"fmt"
+	"net/http"
 
 	"github.com/SelfServiceCo/api/pkg/models"
+	"github.com/gin-gonic/gin"
 )
 
 func GetRestaurantTables(resID int64, tableId int64) []models.Table {
@@ -54,4 +56,79 @@ func GetRestaurantTables(resID int64, tableId int64) []models.Table {
 	}
 
 	return tables
+}
+
+func GetTable(tableId int64) (models.Table, gin.H) {
+	var table models.Table
+	db, err := sql.Open("mysql", conf.Name+":"+conf.Password+"@tcp("+conf.Db+":3306)/selfservice")
+
+	if err != nil {
+		return table, gin.H{"error": err.Error()}
+	}
+
+	results, err := db.Query("SELECT * FROM tables WHERE table_id = ?", tableId)
+	defer db.Close()
+
+	if err != nil {
+		fmt.Println("Err", err.Error())
+		return table, gin.H{"error": err.Error()}
+	}
+
+	
+	for results.Next() {
+		err = results.Scan(&table.ID, &table.ResID, &table.QR)
+		if err != nil {
+			return table, gin.H{"error": err.Error()}
+		}
+	}
+
+	return table, gin.H{"status": http.StatusOK, "message": "success", "data": results}
+}
+
+func OrdersByTable(tableId int64) ([]models.CustomQuery, gin.H) {
+
+	db, err := sql.Open("mysql", conf.Name+":"+conf.Password+"@tcp("+conf.Db+":3306)/selfservice")
+
+	if err != nil {
+		return nil, gin.H{"status": http.StatusBadRequest, "message": "DB Connection Error! Orders By Table"}
+	}
+
+	results, err := db.Query("SELECT * FROM orders WHERE table_id = ?", tableId)
+	defer db.Close()
+
+	if err != nil {
+		return nil, gin.H{"status": http.StatusBadRequest, "message": "DB Query Error! Orders By Table"}
+	}
+
+	customQuery := []models.CustomQuery{}
+	for results.Next() {
+		var cq models.CustomQuery
+		err = results.Scan(&cq.OrderItemID, &cq.TableID, &cq.ProductID, &cq.Status, &cq.Quantity)
+		if err != nil {
+			return nil, gin.H{"status": http.StatusBadRequest, "message": "DB Scan Error! Orders By Table"}
+		}
+		customQuery = append(customQuery, cq)
+	}
+
+	return customQuery, gin.H{"status": http.StatusOK, "message": "success", "data": results}
+}
+
+func AddTable(table models.Table) (bool, gin.H) {
+
+	db, err := sql.Open("mysql", conf.Name+":"+conf.Password+"@tcp("+conf.Db+":3306)/selfservice")
+
+	if err != nil {
+		fmt.Println("Err", err.Error())
+		return false, gin.H{"status": http.StatusBadRequest, "message": "DB Connection Error! Add Table"}
+	}
+
+	_, err = db.Exec("INSERT INTO tables (rest_id, qr) VALUES (?, ?)", table.ResID, table.QR)
+	defer db.Close()
+
+	if err != nil {
+		fmt.Println("Err", err.Error())
+		return false, gin.H{"status": http.StatusBadRequest, "message": "DB Query Error! Add Table"}
+	}
+
+	return true, gin.H{"status": http.StatusOK, "message": "success"}
 }
