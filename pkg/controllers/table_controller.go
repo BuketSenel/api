49ec1b6bd5805fbf9b2ec7083a7158b7884ba2c0
BuketSenel <buket.senel@ug.bilkent.edu.tr>
2023-04-22 19:57:05 +0300

    TableController :: CreateQRCode Function added

--- pkg/controllers/table_controller.go
+++ pkg/controllers/table_controller.go
@@ -2,8 +2,11 @@ package controllers
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/SelfServiceCo/api/pkg/models"
 	"github.com/gin-gonic/gin"
@@ -19,7 +22,7 @@ func GetRestaurantTables(resID int64, tableId int64) ([]models.Table, gin.H) {
 	}
 
 	if tableId != 0 {
-		results, err := db.Query("SELECT rest_id, table_no, waiter_no, qr FROM tables WHERE rest_id = ? AND table_no = ?", resID, tableId)
+		results, err := db.Query("SELECT rest_id, table_no, waiter_id FROM tables WHERE rest_id = ? AND table_no = ?", resID, tableId)
 		defer db.Close()
 
 		if err != nil {
@@ -28,7 +31,7 @@ func GetRestaurantTables(resID int64, tableId int64) ([]models.Table, gin.H) {
 		tables := []models.Table{}
 		for results.Next() {
 			var table models.Table
-			err = results.Scan(&table.RestID, &table.TableNo, &table.WaiterID, &table.QR)
+			err = results.Scan(&table.RestID, &table.TableNo, &table.WaiterID)
 			if err != nil {
 				return nil, gin.H{"status": http.StatusBadRequest, "message": "Scan Error! Get Restaurant Tables"}
 			}
@@ -37,7 +40,7 @@ func GetRestaurantTables(resID int64, tableId int64) ([]models.Table, gin.H) {
 
 		return tables, gin.H{"status": http.StatusOK, "message": "success", "data": tables}
 	}
-	results, err := db.Query("SELECT * FROM tables WHERE rest_id = ?", resID)
+	results, err := db.Query("SELECT rest_id, table_no, waiter_id FROM tables WHERE rest_id = ?", resID)
 	defer db.Close()
 
 	if err != nil {
@@ -46,7 +49,7 @@ func GetRestaurantTables(resID int64, tableId int64) ([]models.Table, gin.H) {
 	tables := []models.Table{}
 	for results.Next() {
 		var table models.Table
-		err = results.Scan(&table.RestID, &table.TableNo, &table.WaiterID, &table.QR)
+		err = results.Scan(&table.RestID, &table.TableNo, &table.WaiterID)
 		if err != nil {
 			return nil, gin.H{"status": http.StatusBadRequest, "message": "Scan Error! Get Restaurant Tables"}
 		}
@@ -126,12 +129,12 @@ func AddTable(c *gin.Context) (bool, gin.H) {
 	}
 
 	var header = gin.H{}
-	table.QR, header = CreateQRCode(table.TableNo, table.RestID)
+	qr_png, header := CreateQRCode(table.TableNo, table.RestID)
 	if header["status"] != http.StatusOK {
-		return false, header
+		return false, gin.H{"status": http.StatusBadRequest, "message": "QR Code Error! Add Table", "error": header, "data": qr_png}
 	}
 
-	_, err = db.Exec("INSERT INTO tables (table_no, rest_id, qr) VALUES (?, ?, ?)", table.TableNo, table.RestID, table.QR)
+	_, err = db.Exec("INSERT INTO tables (table_no, rest_id) VALUES (?, ?)", table.TableNo, table.RestID)
 	defer db.Close()
 
 	if err != nil {
@@ -139,17 +142,34 @@ func AddTable(c *gin.Context) (bool, gin.H) {
 		return false, gin.H{"status": http.StatusBadRequest, "message": "DB Query Error! Add Table", "error": err.Error()}
 	}
 
-	return true, gin.H{"status": http.StatusOK, "message": "success", "qr": table.QR}
+	return true, gin.H{"status": http.StatusOK, "message": "success", "qr": qr_png}
 }
 
-func CreateQRCode(tid int64, rid int64) ([]byte, gin.H) {
+func CreateQRCode(tid int64, rid int64) (string, gin.H) {
 	var table = models.Table{}
 	var err error
-	table.QR, err = qrcode.Encode(string(tid)+":"+string(rid), qrcode.Medium, 256)
+	qr_string := map[string]int64{"table_no": tid, "rest_id": rid}
+	qr_json, _ := json.Marshal(qr_string)
+	table.QR, err = qrcode.New(string(qr_json), qrcode.Medium)
 	if err != nil {
-		return nil, gin.H{"status": http.StatusBadRequest, "message": "QR Code Error! Create QR Code"}
+		return "", gin.H{"status": http.StatusBadRequest, "message": "QR Code Error! Create QR Code"}
 	}
-	return table.QR, gin.H{"status": http.StatusOK, "message": "success"}
+	if err != nil {
+		return "", gin.H{"status": http.StatusBadRequest, "message": "Could not retrive the file from the request!"}
+	}
+	filePath := "/www/qr-codes/" + strconv.FormatInt(rid, 10) + "/" + strconv.FormatInt(tid, 10) + ".png"
+	if _, err := os.Stat("/www/qr-codes/" + strconv.FormatInt(rid, 10)); os.IsNotExist(err) {
+		os.Mkdir("/www/qr-codes/"+strconv.FormatInt(rid, 10), 0777)
+	}
+	osFile, err := os.Create(filePath)
+	if err != nil {
+		return "", gin.H{"status": http.StatusBadRequest, "message": "Could not create the file!", "error": err}
+	}
+	err = table.QR.WriteFile(256, osFile.Name())
+	if err != nil {
+		return "", gin.H{"status": http.StatusBadRequest, "message": "QR Code Write File Error! Create QR Code", "error": err.Error()}
+	}
+	return filePath, gin.H{"status": http.StatusOK, "message": "success"}
 }
 
 func EditTable(c *gin.Context) (bool, gin.H) {
--- pkg/models/table.go
+++ pkg/models/table.go
@@ -1,10 +1,12 @@
 package models
 
+import "github.com/skip2/go-qrcode"
+
 type Table struct {
-	TableNo    int64  `json:"table_no"`
-	RestID     int64  `json:"rest_id"`
-	WaiterID   int64  `json:"waiter_id"`
-	QR         []byte `json:"qr"`
-	NewTableNo int64  `json:"new_table_no"`
-	WaiterName string `json:"waiter_name"`
+	TableNo    int64          `json:"table_no"`
+	RestID     int64          `json:"rest_id"`
+	WaiterID   int64          `json:"waiter_id"`
+	QR         *qrcode.QRCode `json:"qr"`
+	NewTableNo int64          `json:"new_table_no"`
+	WaiterName string         `json:"waiter_name"`
 }
